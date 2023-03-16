0decab4e2783d3b3d18fbf84b89bd883c775d9d7
Varun Potti <potti.varun07@gmail.com>
2023-03-16 11:09:03 +0530

    Update App.tsx

--- components/App/App.tsx
+++ components/App/App.tsx
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 
 const App = () => {
+  useEffect(()=>{
+    window.location.replace("https://apisummary.notion.site/0821f3f195c44bff9b9adff8c56e56ad?v=5df958f85f944fb7bd477cc3c6810877")  
+  }, [])
   return <div>Welcome to Next.js!</div>;
 };
 
